1fcc8cb8e67e9e59dc208a6aa4b106a1f63635a9
Daniel Sperka <djsperka@ucdavis.edu>
2020-03-25 09:09:46 -0700

    Set env variables prior to starting cmake

---
+++ win32-build-env.bat
@@ -0,0 +1,6 @@
+REM Set these for your machine. I am using netcdf distribution 
+REM for zlib and hdf5, but I copied the hdf5 include folder to netcdf 
+REM folder, as the netcdf distribution does not include hdf headers. 
+SET NETCDF_ROOT=c:/netCDF-4.7.3
+SET HDF5_ROOT=c:/netCDF-4.7.3
+SET ZLIB_ROOT=c:/netCDF-4.7.3
